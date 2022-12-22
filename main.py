ef592186aa59dbef8e1cfadbf28b87024e9a4a20
Maria Joao Gomes <mariajoao.mjg17@gmail.com>
2022-12-22 18:52:34 +0000

    Commit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tkinter import messagebox
 from PIL import ImageTk,Image  
 import os
 
+
+
 window=Tk()
 
 global screenHeight
@@ -20,6 +22,13 @@ x = (screenWidth/2) - (appWidth/2)
 y = (screenHeight/2) - (appHeight/2)
 window.geometry("{:.0f}x{:.0f}+{:.0f}+{:.0f}" .format(appWidth, appHeight, int(x), int(y)))
 
-window.title("")
+window.title("ToDoList")
+
+#Botões de conta
+btnInciarSessao = Button(window, width = 20, height= 2, text = "Iniciar Sessões", bd=1, fg='black', relief = "raised")
+btnInciarSessao.place(x=500, y=0)
+
+btnCriarConta = Button(window, width = 20, height= 2, text = "Criar Conta", bd=1, fg='black', relief = "raised")
+btnCriarConta.place(x=650, y=0)
 
 window.mainloop()
